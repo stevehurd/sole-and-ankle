2dd19424f9ddd16612e939e4793cdda2a4aae677
Steve Hurd <660233+stevehurd@users.noreply.github.com>
2022-03-11 18:15:01 -0700

    Module 4 styling

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,58 +1,70 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<MainColumn>
+				<Header>
+					<Title>Running</Title>
+					<Select
+						label="Sort"
+						value={sortId}
+						onChange={(ev) => setSortId(ev.target.value)}
+					>
+						<option value="newest">Newest Releases</option>
+						<option value="price">Price</option>
+					</Select>
+				</Header>
+				<Spacer size={34} />
+				<ShoeGrid />
+			</MainColumn>
+			<LeftColumn>
+				<Breadcrumbs>
+					<Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+				</Breadcrumbs>
+				<Spacer size={42} />
+				<ShoeSidebar />
+			</LeftColumn>
+		</Wrapper>
+	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row-reverse;
+	justify-content: space-between;
+	align-items: baseline;
+	gap: 48px;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+	position: sticky;
+	top: 40px;
+`;
 
 const MainColumn = styled.div``;
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.5rem;
+	font-weight: ${WEIGHTS.medium};
 `;
 
 export default ShoeIndex;
